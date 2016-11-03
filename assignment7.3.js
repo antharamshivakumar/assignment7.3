86a6a6152cc582db0318ac7335760c514b36f453
antharamshivakumar <antharamshivakumar@gmail.com>
2016-11-03 12:24:49 -0700

    Add files via upload

---
+++ assignment7.3.js
@@ -0,0 +1,14 @@
+function add (n1,n2) {
+    
+    var name = "Counter";
+    
+    function addMore () {
+        
+    console.log(" Value of name is " + name);
+    console.log("You passed 2 varialbes n1 and n2" + n1 + "," + n2);    
+        
+        
+    }
+    
+    addMore();
+}
